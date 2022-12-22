111dc474289f8754ab73c42e676b2d5fd073ad18
Rafly Maulana <mraflymaulana@gmail.com>
2022-12-23 01:35:53 +0700

    Add page transition progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import RouterTransition from "@/components/RouterTransition";
+
 import { Manrope } from "@next/font/google";
+// eslint-disable-next-line import/named
+import { MantineProvider } from "@mantine/core";
 
 const manrope = Manrope({
     variable: "--font-manrope",
@@ -12,8 +16,11 @@ const manrope = Manrope({
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <main className={`${manrope.variable} font-sans`}>
-            <Component {...pageProps} />
-        </main>
+        <MantineProvider withCSSVariables withGlobalStyles withNormalizeCSS>
+            <main className={`${manrope.variable} font-sans`}>
+                <RouterTransition />
+                <Component {...pageProps} />
+            </main>
+        </MantineProvider>
     );
 }
