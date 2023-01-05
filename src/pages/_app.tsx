48ebbeccc367f396df31bbfd2d33cd565a3f5bd8
Rafly Maulana <mraflymaulana@gmail.com>
2023-01-06 02:02:41 +0700

    Adjust font settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,13 @@ import { Manrope } from "@next/font/google";
 
 const manrope = Manrope({
     variable: "--font-manrope",
+    preload: true,
+    adjustFontFallback: true,
+    display: "swap",
+    fallback: ["sans-serif"],
     weight: ["400", "500", "600", "700"],
     style: ["normal"],
-    subsets: ["latin"],
+    subsets: ["latin", "latin-ext"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
