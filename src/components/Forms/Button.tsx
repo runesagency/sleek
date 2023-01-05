55727ab0ca1315b4e3fe2c4ba55b8416d1f5bd41
Rafly Maulana <mraflymaulana@gmail.com>
2023-01-05 19:08:44 +0700

    Fix shared button props types

--- src/components/Forms/Button.tsx
+++ src/components/Forms/Button.tsx
@@ -1,13 +1,13 @@
-import type { HTMLProps } from "react";
+import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 import type { TablerIcon } from "@tabler/icons";
 
-type ButtonProps = HTMLProps<HTMLButtonElement> & {
+type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
     icon?: TablerIcon;
 };
 
-export const Small = ({ children, className, icon: Icon }: ButtonProps) => {
+export const Small = ({ children, className, icon: Icon, ...props }: ButtonProps) => {
     return (
-        <button className={`flex w-max items-center justify-start gap-2 rounded-lg bg-dark-600 px-3 py-1 text-sm font-bold text-dark-50 duration-200 hover:opacity-50 ${className}`}>
+        <button className={`flex w-max items-center justify-center gap-2 rounded-lg bg-dark-600 px-3 py-1 text-sm font-bold text-dark-50 duration-200 hover:opacity-50 ${className}`} {...props}>
             {Icon && <Icon height={16} width={undefined} className="shrink-0" />}
             {children}
         </button>
