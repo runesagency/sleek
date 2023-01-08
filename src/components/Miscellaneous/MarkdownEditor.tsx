f2beceb0c9a65bddbb025f712b5b7d1cea914186
Rafly Maulana <mraflymaulana@gmail.com>
2023-01-08 23:36:46 +0700

    Allow get Mde instance by ref

--- src/components/Miscellaneous/MarkdownEditor.tsx
+++ src/components/Miscellaneous/MarkdownEditor.tsx
@@ -2,6 +2,7 @@ import "easymde/dist/easymde.min.css";
 
 import type { Options as EasyMDEOptions } from "easymde";
 import type { SimpleMDEReactProps } from "react-simplemde-editor";
+import type EasyMDE from "easymde";
 
 import dynamic from "next/dynamic";
 import { useCallback, useMemo, useState } from "react";
@@ -9,14 +10,14 @@ import { useCallback, useMemo, useState } from "react";
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
 
 type MarkdownEditorProps = {
+    innerRef?: React.MutableRefObject<EasyMDE | null>;
     defaultValue?: string;
     placeholder?: string;
     options?: EasyMDEOptions;
     onChange?: SimpleMDEReactProps["onChange"];
-    onMount?: (instance: EasyMDE) => void;
 };
 
-export default function MarkdownEditor({ defaultValue, onChange: onInputChange, placeholder, options, onMount: onComponentMount }: MarkdownEditorProps) {
+export default function MarkdownEditor({ defaultValue, onChange: onInputChange, placeholder, options, innerRef }: MarkdownEditorProps) {
     const [value, setValue] = useState(defaultValue);
 
     const onChange = useCallback(
@@ -36,5 +37,17 @@ export default function MarkdownEditor({ defaultValue, onChange: onInputChange, 
         [options]
     );
 
-    return <SimpleMDE value={value} onChange={onChange} options={mdEditorOptions} placeholder={placeholder ?? undefined} />;
+    return (
+        <SimpleMDE
+            value={value}
+            onChange={onChange}
+            options={mdEditorOptions}
+            placeholder={placeholder ?? undefined}
+            getMdeInstance={(instance) => {
+                if (innerRef) {
+                    innerRef.current = instance;
+                }
+            }}
+        />
+    );
 }
