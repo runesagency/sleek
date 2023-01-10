b7c837ac935d542fda066e3c5d4d9cd9ef6c9ad5
Rafly Maulana <mraflymaulana@gmail.com>
2023-01-10 12:30:01 +0700

    Use next-connect for building better API

--- src/pages/api/cards/[id]/index.ts
+++ src/pages/api/cards/[id]/index.ts
@@ -3,51 +3,66 @@ import type { Card as CardType } from "@/lib/types";
 
 import { prisma } from "@/lib/prisma";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+import nc from "next-connect";
+
+const router = nc<NextApiRequest, NextApiResponse>();
+
+/**
+ * GET /api/cards/[id]
+ * @summary Get a card
+ */
+router.get(async (req, res) => {
+    const { id } = req.query as { id: string };
+
+    const card = await prisma.cards.findUnique({
+        where: { id },
+    });
+
+    if (!card) {
+        return res.status(404).send("Card not found");
+    }
+
+    return res.status(200).json(card);
+});
+
+/**
+ * PATCH /api/cards/[id]
+ * @summary Edit a card data
+ * @param {CardType} card - A card object with updated data
+ */
+router.patch(async (req, res) => {
     const { id } = req.query as { id: string };
     const card: CardType = req.body;
 
-    switch (req.method) {
-        case "GET": {
-            const card = await prisma.cards.findUnique({
-                where: { id },
-            });
+    const newCard = await prisma.cards.update({
+        where: { id },
+        data: {
+            title: card.title,
+            description: card.description,
+            order: card.order,
+            list_id: card.list_id,
+            board_id: card.board_id,
+            cover_attachment_id: card.cover_attachment_id,
+            start_date: card.start_date,
+            due_date: card.due_date,
+        },
+    });
 
-            if (!card) {
-                return res.status(404).send("Card not found");
-            }
+    return res.status(200).json(newCard);
+});
 
-            return res.status(200).json(card);
-        }
+/**
+ * DELETE /api/cards/[id]
+ * @summary Delete a card
+ */
+router.delete(async (req, res) => {
+    const { id } = req.query as { id: string };
 
-        case "PATCH": {
-            await prisma.cards.update({
-                where: { id },
-                data: {
-                    title: card.title,
-                    description: card.description,
-                    order: card.order,
-                    list_id: card.list_id,
-                    board_id: card.board_id,
-                    cover_attachment_id: card.cover_attachment_id,
-                    start_date: card.start_date,
-                    due_date: card.due_date,
-                },
-            });
+    await prisma.cards.delete({
+        where: { id },
+    });
 
-            return res.status(200).end();
-        }
+    return res.status(200).end();
+});
 
-        case "DELETE": {
-            await prisma.cards.delete({
-                where: { id },
-            });
-
-            return res.status(200).end();
-        }
-
-        default: {
-            return res.status(405).send("Method not allowed");
-        }
-    }
-}
+export default router;
--- src/pages/api/cards/index.ts
+++ src/pages/api/cards/index.ts
@@ -3,92 +3,116 @@ import type { Card as CardType } from "@/lib/types";
 
 import { prisma } from "@/lib/prisma";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+import nc from "next-connect";
+
+const router = nc<NextApiRequest, NextApiResponse>();
+
+/**
+ * POST /api/cards
+ * @summary Create a single or multiple cards
+ * @param {CardType | CardType[]} cards - A card or an array of cards object
+ */
+router.post(async (req, res) => {
     const sourceCards: CardType[] | CardType = req.body;
     const cards = Array.isArray(sourceCards) ? sourceCards : [sourceCards];
 
-    switch (req.method) {
-        case "POST": {
-            const results = cards.map(async (card) => {
-                return await prisma.cards.create({
-                    data: {
-                        title: card.title,
-                        description: card.description,
-                        list_id: card.list_id,
-                        board_id: card.board_id,
-                        order: card.order,
-                        cover_attachment_id: card.cover_attachment_id,
-                        start_date: card.start_date,
-                        due_date: card.due_date,
-                    },
+    const results = cards.map(async (card) => {
+        return await prisma.cards.create({
+            data: {
+                title: card.title,
+                description: card.description,
+                list_id: card.list_id,
+                board_id: card.board_id,
+                order: card.order,
+                cover_attachment_id: card.cover_attachment_id,
+                start_date: card.start_date,
+                due_date: card.due_date,
+            },
+            include: {
+                labels: {
                     include: {
-                        labels: {
-                            include: {
-                                label: true,
-                            },
-                        },
-                        users: {
-                            include: {
-                                user: true,
-                            },
-                        },
-                        activities: {
-                            include: {
-                                user: true,
-                            },
-                        },
-                        attachments: {
-                            include: {
-                                attachment: true,
-                            },
-                        },
-                        cover: {
-                            include: {
-                                attachment: true,
-                            },
-                        },
-                        checklists: {
-                            include: {
-                                checklist: {
-                                    include: {
-                                        tasks: true,
-                                    },
-                                },
-                            },
-                        },
-                        creator: true,
-                        timers: true,
+                        label: true,
                     },
-                });
-            });
-
-            if (results.length === 1) {
-                return res.status(200).json(await results[0]);
-            } else {
-                return res.status(200).json(await Promise.all(results));
-            }
-        }
-
-        case "PATCH": {
-            cards.map(async (card) => {
-                await prisma.cards.update({
-                    where: {
-                        id: card.id,
+                },
+                users: {
+                    include: {
+                        user: true,
                     },
-                    data: {
-                        title: card.title,
-                        description: card.description,
-                        order: card.order,
-                        list_id: card.list_id,
+                },
+                activities: {
+                    include: {
+                        user: true,
                     },
-                });
-            });
+                },
+                attachments: {
+                    include: {
+                        attachment: true,
+                    },
+                },
+                cover: {
+                    include: {
+                        attachment: true,
+                    },
+                },
+                checklists: {
+                    include: {
+                        checklist: {
+                            include: {
+                                tasks: true,
+                            },
+                        },
+                    },
+                },
+                creator: true,
+                timers: true,
+            },
+        });
+    });
 
-            return res.status(200).end();
-        }
+    return res.status(200).json(results.length === 1 ? await results[0] : await Promise.all(results));
+});
 
-        default: {
-            return res.status(405).end();
-        }
-    }
-}
+/**
+ * PATCH /api/cards
+ * @summary Edit multiple cards data
+ * @param {CardType[]} cards - An array of cards object with updated data
+ */
+router.patch(async (req, res) => {
+    const cards: CardType[] = req.body;
+
+    cards.map(async (card) => {
+        await prisma.cards.update({
+            where: {
+                id: card.id,
+            },
+            data: {
+                title: card.title,
+                description: card.description,
+                order: card.order,
+                list_id: card.list_id,
+            },
+        });
+    });
+
+    return res.status(200).end();
+});
+
+/**
+ * DELETE /api/cards
+ * @summary Delete multiple cards
+ */
+router.delete(async (req, res) => {
+    const cards: CardType[] = req.body;
+
+    await prisma.cards.deleteMany({
+        where: {
+            id: {
+                in: cards.map((card) => card.id),
+            },
+        },
+    });
+
+    return res.status(200).end();
+});
+
+export default router;
