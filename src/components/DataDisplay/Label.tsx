30252c63351c9c8467be7ab55cca884d23cee2d8
Rafly Maulana <mraflymaulana@gmail.com>
2023-01-15 11:30:34 +0700

    Make color props nullable instead undefined-able

--- src/components/DataDisplay/Label.tsx
+++ src/components/DataDisplay/Label.tsx
@@ -1,9 +1,9 @@
-export default function Label({ name, color, className }: { name: string; color?: string; className?: string }) {
+export default function Label({ name, color, className }: { name: string; color: string | null; className?: string }) {
     return (
         <div
             className={`rounded-full bg-dark-800 px-2 py-1 text-xs ${className}`}
             style={{
-                backgroundColor: color,
+                backgroundColor: color || undefined,
             }}
         >
             {name}
