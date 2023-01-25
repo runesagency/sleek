e493cb3aecb2c0aba497d93acf36ce06902e1eea
Rafly Maulana <mraflymaulana@gmail.com>
2023-01-26 02:55:06 +0700

    Create member list component

---
+++ src/components/DataDisplay/MemberList.tsx
@@ -0,0 +1,66 @@
+import type { users as User } from "@prisma/client";
+
+import Avatar from "@/components/Miscellaneous/Avatar";
+
+import { memo } from "react";
+
+type MemberListProps = {
+    users: User[];
+    max?: number;
+    className?: string;
+};
+
+const Small = ({ users, max }: MemberListProps) => {
+    return (
+        <div className="flex shrink-0 -space-x-1">
+            {users.map((user, i) => {
+                if (!user || (max && i > max)) return null;
+
+                const title = max ? (i !== max - 1 ? user.name : `+${users.length - max}`) : "";
+
+                return (
+                    <Avatar
+                        key={i} //
+                        alt={title}
+                        title={title}
+                        className="h-6 w-6 rounded-full border border-dark-600 object-cover object-center"
+                        config={{
+                            seed: title,
+                        }}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+const Large = ({ users, max }: MemberListProps) => {
+    return (
+        <div className="flex shrink-0 -space-x-2">
+            {users.map((user, i) => {
+                if (!user || (max && i > max)) return null;
+
+                const title = max ? (i !== max - 1 ? user.name : `+${users.length - max}`) : "";
+
+                return (
+                    <Avatar
+                        key={i} //
+                        alt={title}
+                        title={title}
+                        className="h-10 w-10 rounded-full border border-dark-600 object-cover object-center"
+                        config={{
+                            seed: title,
+                        }}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+const MemberList = {
+    Small: memo(Small),
+    Large: memo(Large),
+};
+
+export default MemberList;
