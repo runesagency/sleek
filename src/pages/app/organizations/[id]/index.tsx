fc2d86c6d678a4f807e224da75103bef2e252fe8
Rafly Maulana <mraflymaulana@gmail.com>
2023-01-25 14:30:45 +0700

    Move page to it's actual route

--- src/pages/app/organizations/[id]/index.tsx
+++ src/pages/app/organizations/[id]/index.tsx
@@ -1,12 +1,18 @@
 import OrganizationPageLayout from "@/components/Dashboard/OrganizationPageLayout";
 import Button from "@/components/Forms/Button";
 
-import { IconCalendar, IconCards } from "@tabler/icons";
+import { IconCalendar, IconCards, IconFolder } from "@tabler/icons";
 
 export default function DashboardPage() {
     return (
-        <OrganizationPageLayout className="flex flex-col gap-10 ">
+        <OrganizationPageLayout className="flex flex-col gap-10">
             {/* Folders (Future Development) */}
+            <div className="flex flex-wrap gap-5">
+                <button className="flex items-center gap-4 truncate rounded-lg border border-dark-500 bg-dark-600 px-5 py-4">
+                    <IconFolder height={20} width={undefined} />
+                    <span>Folder Name</span>
+                </button>
+            </div>
 
             {/* Project List */}
             <div className="grid grid-cols-3 gap-5">
