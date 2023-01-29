0d5fc36d87e79b84b7efeb3acb149ffade71fb8c
Rafly Maulana <mraflymaulana@gmail.com>
2023-01-29 21:24:26 +0700

    Create array-move.ts

---
+++ src/lib/utils/array-move.ts
@@ -0,0 +1,18 @@
+export const arrayMoveMutable = <T extends unknown[]>(array: T, fromIndex: number, toIndex: number) => {
+    const startIndex = fromIndex < 0 ? array.length + fromIndex : fromIndex;
+
+    if (startIndex >= 0 && startIndex < array.length) {
+        const endIndex = toIndex < 0 ? array.length + toIndex : toIndex;
+
+        const [item] = array.splice(fromIndex, 1);
+        array.splice(endIndex, 0, item);
+    }
+};
+
+export const arrayMoveImmutable = <T extends unknown[]>(array: T, fromIndex: number, toIndex: number) => {
+    const newArray = [...array];
+
+    arrayMoveMutable(newArray, fromIndex, toIndex);
+
+    return newArray as T;
+};
