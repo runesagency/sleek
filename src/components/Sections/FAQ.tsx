ec21a7d958174a41c8a7f0c763a30cad6f52c8a4
Rafly Maulana <mraflymaulana@gmail.com>
2023-02-07 18:14:02 +0700

    Fix and refactoring

--- src/components/Sections/FAQ.tsx
+++ src/components/Sections/FAQ.tsx
@@ -1,71 +1,59 @@
-import { IconChevronDown, IconChevronUp } from "@tabler/icons";
-import { useState } from "react";
+import Container from "@/components/Sections/Container";
 
-export default function FAQ() {
-    const dummyFaqData = [
-        {
-            id: "1",
-            question: "What does LOREM mean?",
-            answer: "‘Lorem ipsum dolor sit amet, consectetur adipisici elit…’ (complete text) is dummy text that is not meant to mean anything. It is used as a placeholder in magazine layouts, for example, in order to give an impression of the finished document. The text is intentionally unintelligible so that the viewer is not distracted by the content. The language is not real Latin and even the first word ‘Lorem’ does not exist. It is said that the lorem ipsum text has been common among typesetters since the 16th century. (Source: Wikipedia.com).",
-        },
-        {
-            id: "2",
-            question: "Where can I subscribe to your newsletter?",
-            answer: "‘Lorem ipsum dolor sit amet, consectetur adipisici elit…’ (complete text) is dummy text that is not meant to mean anything. It is used as a placeholder in magazine layouts, for example, in order to give an impression of the finished document. The text is intentionally unintelligible so that the viewer is not distracted by the content. The language is not real Latin and even the first word ‘Lorem’ does not exist. It is said that the lorem ipsum text has been common among typesetters since the 16th century. (Source: Wikipedia.com).",
-        },
-        {
-            id: "3",
-            question: "Where can I edit my billing and shipping address?",
-            answer: "‘Lorem ipsum dolor sit amet, consectetur adipisici elit…’ (complete text) is dummy text that is not meant to mean anything. It is used as a placeholder in magazine layouts, for example, in order to give an impression of the finished document. The text is intentionally unintelligible so that the viewer is not distracted by the content. The language is not real Latin and even the first word ‘Lorem’ does not exist. It is said that the lorem ipsum text has been common among typesetters since the 16th century. (Source: Wikipedia.com).",
-        },
-        {
-            id: "4",
-            question: "Can I order a free sample copy of a magazine?",
-            answer: "‘Lorem ipsum dolor sit amet, consectetur adipisici elit…’ (complete text) is dummy text that is not meant to mean anything. It is used as a placeholder in magazine layouts, for example, in order to give an impression of the finished document. The text is intentionally unintelligible so that the viewer is not distracted by the content. The language is not real Latin and even the first word ‘Lorem’ does not exist. It is said that the lorem ipsum text has been common among typesetters since the 16th century. (Source: Wikipedia.com).",
-        },
-        {
-            id: "5",
-            question: "Are unsold magazines sent back to the publisher?",
-            answer: "‘Lorem ipsum dolor sit amet, consectetur adipisici elit…’ (complete text) is dummy text that is not meant to mean anything. It is used as a placeholder in magazine layouts, for example, in order to give an impression of the finished document. The text is intentionally unintelligible so that the viewer is not distracted by the content. The language is not real Latin and even the first word ‘Lorem’ does not exist. It is said that the lorem ipsum text has been common among typesetters since the 16th century. (Source: Wikipedia.com).",
-        },
-        {
-            id: "6",
-            question: "We would like to sell our magazine through your shop but we haven’t yet received an answer",
-            answer: "‘Lorem ipsum dolor sit amet, consectetur adipisici elit…’ (complete text) is dummy text that is not meant to mean anything. It is used as a placeholder in magazine layouts, for example, in order to give an impression of the finished document. The text is intentionally unintelligible so that the viewer is not distracted by the content. The language is not real Latin and even the first word ‘Lorem’ does not exist. It is said that the lorem ipsum text has been common among typesetters since the 16th century. (Source: Wikipedia.com).",
-        },
-    ];
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { IconChevronDown } from "@tabler/icons";
+import { useCallback, useState } from "react";
 
-    const [isOpens, setIsOpens] = useState<string[]>([]);
+type QnAProps = {
+    question: string;
+    answer: string;
+};
 
-    const checkOpen = (id: string) => {
-        for (let index = 0; index < isOpens.length; index++) {
-            if (isOpens[index] === id) {
-                return true;
-            }
-        }
-        return false;
-    };
+const QnA = ({ question, answer }: QnAProps) => {
+    const [ref] = useAutoAnimate();
+    const [isOpen, setIsOpen] = useState(false);
 
-    const handleClick = (id: string) => {
-        if (checkOpen(id)) {
-            setIsOpens((curr) => curr.filter((elem) => elem !== id));
-        } else {
-            setIsOpens((curr) => [...curr, id]);
-        }
-    };
+    const onOpen = useCallback(() => {
+        setIsOpen(true);
+    }, []);
+
+    const onClose = useCallback(() => {
+        setIsOpen(false);
+    }, []);
 
     return (
-        <div className="mx-auto columns-2 gap-9 space-y-5">
-            {dummyFaqData.map((val, idx) => {
-                let status = checkOpen(val.id);
-                return (
-                    <div key={idx} onClick={() => handleClick(val.id)} className={`flex max-w-lg cursor-pointer justify-between gap-4 rounded-lg bg-dark-700 px-7 py-5 ${status ? "flex-wrap" : ""}`}>
-                        <h1 className="flex-initial basis-11/12 text-lg">{val.question}</h1>
-                        <IconChevronUp className={`flex w-5 flex-initial flex-col justify-center duration-150 ease-in-out ${status ? "rotate-180" : ""}`} />
-                        <h1 className={`text-base ${status ? "block duration-300" : "hidden"}`}>{val.answer}</h1>
-                    </div>
-                );
-            })}
+        <div ref={ref} onClick={!isOpen ? onOpen : undefined} className="flex w-full cursor-pointer flex-col gap-4 rounded-lg border border-dark-600 bg-dark-700 px-7 py-5">
+            <div onClick={!isOpen ? undefined : onClose} className="flex items-center gap-4">
+                <h4 className="ts-xl flex-1">{question}</h4>
+
+                <IconChevronDown className={`flex w-5 shrink-0 duration-200 ${isOpen ? "rotate-180" : "rotate-0"}`} />
+            </div>
+
+            {isOpen && <p className="ts-base cursor-text text-justify">{answer}</p>}
         </div>
+    );
+};
+
+type FAQProps = {
+    data: QnAProps[];
+};
+
+export default function FAQ({ data }: FAQProps) {
+    return (
+        <Container className={["bg-dark-900", "flex flex-col gap-14 px-48 py-20"]}>
+            <h3 className="heading-3 text-center">Have Any Questions?</h3>
+
+            <section className="mx-auto grid w-full max-w-7xl grid-cols-2 gap-9">
+                {[...Array(2)].map((_, colIndex) => (
+                    <div key={colIndex} className="flex flex-col gap-9">
+                        {data
+                            .filter((_, index) => index % 2 === colIndex)
+                            .map((qna, index) => (
+                                <QnA key={index} {...qna} />
+                            ))}
+                    </div>
+                ))}
+            </section>
+        </Container>
     );
 }
--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,6 +1,7 @@
+import Container from "@/components/Sections/Container";
 import FAQ from "@/components/Sections/FAQ";
 import Footer from "@/components/Sections/Footer";
-import Navbar from "@/components/Sections/Navbar";
+import Navigation from "@/components/Sections/Navigation";
 
 import { IconBuildingStore, IconCheck, IconX } from "@tabler/icons";
 import { useState } from "react";
@@ -80,80 +81,119 @@ function Comparison() {
 
 export default function PricingPage() {
     const [isEnterprise, setIsEnterprise] = useState<boolean>(false);
+
     return (
-        <div>
-            <div>
-                <Navbar />
-            </div>
-            <div>
-                <div className="flex gap-20 bg-dark-800 px-48 py-20 text-dark-50">
-                    <div className="flex shrink-0 basis-3/5 flex-col gap-14">
-                        <div className="flex flex-col gap-7">
-                            <h1 className="text-5xl font-semibold">One Price for All Features, for Small Team or Enterprises.</h1>
-                            <h1 className="w-3/4 text-xl">No more different plans with different features, at Sleek, you only need to subscribe to one plan, and get full access to our app.</h1>
-                        </div>
-                        <div className="flex flex-col gap-5">
-                            <div className="flex gap-5">
-                                <p className="flex flex-col justify-center">Small Team</p>
-                                <div
-                                    onClick={() => setIsEnterprise(!isEnterprise)}
-                                    className={`h-8 w-20 cursor-pointer rounded-2xl border-2 bg-dark-500 ${!isEnterprise ? "bg-dark-400" : "bg-dark-500"}`}
-                                >
-                                    <div className={`mt-1 flex h-5 w-5 flex-col justify-center rounded-full bg-dark-50 duration-150 ease-in-out ${!isEnterprise ? "ml-1" : "ml-12"}`} />
-                                </div>
-                                <p className="flex flex-col justify-center">Enterprise</p>
+        <main className="box-border flex h-full w-full flex-col text-dark-50">
+            <Navigation />
+
+            {/* Hero */}
+            <Container className={["bg-dark-800", "flex gap-20 px-48 py-20"]}>
+                {/* Description */}
+                <aside className="flex flex-col gap-14">
+                    {/* Content */}
+                    <section className="flex flex-col gap-7">
+                        <h1 className="heading-2">One Price for All Features, for Small Team or Enterprises.</h1>
+                        <h2 className="ts-xl w-5/6">No more different plans with different features, at Sleek, you only need to subscribe to one plan, and get full access to our app.</h2>
+                    </section>
+
+                    {/* Company Type */}
+                    <section className="flex flex-col gap-5">
+                        <div className="flex gap-5">
+                            <p className="ts-2xl">Small Team</p>
+
+                            <div
+                                onClick={() => setIsEnterprise(!isEnterprise)}
+                                className={`
+                                    h-8 w-20 cursor-pointer rounded-2xl border-2 bg-dark-500 
+                                    ${!isEnterprise ? "bg-dark-400" : "bg-dark-500"}
+                                `}
+                            >
+                                <div className={`mt-1 flex h-5 w-5 flex-col justify-center rounded-full bg-dark-50 duration-150 ease-in-out ${!isEnterprise ? "ml-1" : "ml-12"}`} />
                             </div>
-                            <h1 className="w-3/4 text-base">
-                                For teams with under 50 members, you will be charged for each member that joins. Whereas for companies with more than 50 users, you will be given a package for the
-                                number of members that can join.
-                            </h1>
+
+                            <p className="ts-2xl">Enterprise</p>
                         </div>
-                    </div>
-                    <div className="flex flex-col justify-center gap-5 rounded-3xl bg-dark-700 px-10 py-8">
-                        <div className="flex flex-col gap-3">
-                            <div className="flex gap-8">
-                                <IconBuildingStore size={48} className="shrink-0" />
-                                <h1 className="flex flex-col justify-center text-4xl font-semibold">Small Team</h1>
+
+                        <p className="ts-base w-4/5">
+                            For teams with under 50 members, you will be charged for each member that joins. Whereas for companies with more than 50 users, you will be given a package for the number
+                            of members that can join.
+                        </p>
+                    </section>
+                </aside>
+
+                {/* Package */}
+                <aside className="w-full max-w-xl">
+                    <div className="sticky top-8 flex h-max w-full max-w-xl flex-col justify-center gap-8 rounded-3xl bg-dark-700 py-8 px-10">
+                        <section className="flex flex-col gap-3">
+                            <div className="flex items-center gap-8">
+                                <IconBuildingStore size={48} width={undefined} className="shrink-0" />
+                                <h2 className="heading-4">Small Team</h2>
                             </div>
-                            <h1 className=" text-xl">A package dedicated to small teams with under 50 members.</h1>
-                        </div>
-                        <div>
-                            <div className="h-2 w-full rounded-xl bg-dark-50">
-                                <div className="relative -top-1.5 h-5 w-5 cursor-pointer rounded-full bg-dark-500" />
+
+                            <h1 className="ts-xl">A package dedicated to small teams with under 50 members.</h1>
+                        </section>
+
+                        <section className="flex flex-col gap-2">
+                            <div className="flex h-2 w-full items-center rounded-lg bg-dark-50">
+                                <div className="h-8 w-8 translate-x-2/3 cursor-pointer rounded-full bg-dark-50" />
                             </div>
-                        </div>
-                        <div>
-                            <h1 className="text-base">Get full access for 15 team members at just:</h1>
-                            <div className="flex">
-                                <h1 className="text-5xl font-semibold">
-                                    USD$ 49.99 <span className="text-base">/ Month</span>
-                                </h1>
+
+                            <div className="ts-sm flex items-center justify-between">
+                                <span>1</span>
+                                <span>50</span>
                             </div>
-                        </div>
+                        </section>
+
+                        <section className="flex flex-col">
+                            <p className="ts-base">Get full access for 15 team members at just:</p>
+
+                            <div className="flex items-end gap-1">
+                                <h3 className="heading-2">USD$ 49.99</h3>
+                                <span className="ts-base">/ Month</span>
+                            </div>
+                        </section>
                     </div>
-                </div>
-                {/* Comparison */}
-                <div className="bg-dark-900 py-20 px-48 text-dark-50">
-                    <div className="flex flex-col gap-14">
-                        <div>
-                            <h1 className="text-center text-3xl font-semibold">Ipsum vs Wokwow: How Do They Compare?</h1>
-                        </div>
-                        <Comparison />
-                    </div>
-                </div>
-                {/* FAQ */}
-                <div className="bg-dark-900 py-20 px-48 text-dark-50">
-                    <div className="flex flex-col gap-14">
-                        <div>
-                            <h1 className="text-center text-3xl font-semibold">Have Any Questions?</h1>
-                        </div>
-                        <FAQ />
-                    </div>
-                </div>
-            </div>
-            <div>
-                <Footer />
-            </div>
-        </div>
+                </aside>
+            </Container>
+
+            {/* Comparison */}
+            <Container className={["bg-dark-900", "flex flex-col gap-14 px-48 py-20"]}>
+                <h3 className="heading-3 text-center">Ipsum vs Wokwow: How Do They Compare?</h3>
+
+                <Comparison />
+            </Container>
+
+            {/* FAQ */}
+            <FAQ
+                data={[
+                    {
+                        question: "What is Lorem Ipsum?",
+                        answer: "Lorem ipsum dolor sit amet, consectetur adipisici elit…’ (complete text) is dummy text that is not meant to mean anything. It is used as a placeholder in magazine layouts, for example, in order to give an impression of the finished document. The text is intentionally unintelligible so that the viewer is not distracted by the content. The language is not real Latin and even the first word Lorem’ does not exist. It is said that the lorem ipsum text has been common among typesetters since the 16th century. (Source: Wikipedia.com).",
+                    },
+                    {
+                        question: "Where can I subscribe to your newsletter?",
+                        answer: "We often send out our newsletter with news and great offers. We will never disclose your data to third parties and you can unsubscribe from the newsletter at any time. Subscribe here to our newsletter.",
+                    },
+                    {
+                        question: "Where can I edit my billing and shipping address?",
+                        answer: "If you created a new account after or while ordering you can edit both addresses (for billing and shipping) in your customer account.",
+                    },
+                    {
+                        question: "Can I order a free sample copy of a magazine?",
+                        answer: "Unfortunately, we’re unable to offer free samples. As a retailer, we buy all magazines from their publishers at the regular trade price. However, you could contact the publisher directly.",
+                    },
+                    {
+                        question: "Are unsold magazines sent back to the publisher?",
+                        answer: "We usually sell all copies of the magazines offered in our shop. Some publishers and distributors offer the retailer the option of returning any unsold magazines. However, because our range includes magazines from countries such as Australia, the USA and the United Kingdom, sending them back would involve considerable effort in terms of logistics and would also be very expensive. We therefore choose not to return any unsold magazines. At the same time, this allows us to also offer our customers older or out-of-print magazines.",
+                    },
+                    {
+                        question: "We would like to sell our magazine through your shop but we haven’t yet received an answer",
+                        answer: "We receive up to 20 enquiries per week from publishers all around the world. Because we can’t always respond to each one right away, all enquiries are checked and answered in chronological order. It may therefore take several weeks or months from receiving your enquiry to being able to respond to it. However, we always endeavour to answer all enquiries.",
+                    },
+                ]}
+            />
+
+            <Footer />
+        </main>
     );
 }
