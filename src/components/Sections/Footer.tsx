7177ea506616350a974d7699fba1294d50ec925e
Rafly Maulana <mraflymaulana@gmail.com>
2023-02-07 14:51:35 +0700

    Fix navbar and footer

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,48 +1,85 @@
 import Button from "@/components/Forms/Button";
+import Input from "@/components/Forms/Input";
 
 import { IconBrandDiscord, IconBrandTwitter, IconMail } from "@tabler/icons";
+import Link from "next/link";
+
+type FooterSectionProps = {
+    title: string;
+    links: {
+        title: string;
+        href: string;
+    }[];
+};
+
+const FooterSection = ({ title, links }: FooterSectionProps) => {
+    return (
+        <section className="flex w-32 shrink-0 flex-col gap-4">
+            <h4 className="ts-xl">Product</h4>
+
+            {links.map(({ title, href }, index) => (
+                <Link key={index} href={href} className="ts-base">
+                    {title}
+                </Link>
+            ))}
+        </section>
+    );
+};
 
 export default function Footer() {
     return (
-        <div className="flex justify-around bg-dark-700 px-48 py-14 text-dark-50">
-            <div className="flex basis-2/12 flex-col gap-5">
-                <img src="/logoipsum-286.svg" className="text-white" alt="logo_ipsum" width="174" height="32" />
-                <h1 className="text-base">Sleek is the most flexible project management app for your team.</h1>
-                <div className="flex gap-7">
-                    <IconBrandTwitter />
-                    <IconBrandDiscord />
-                </div>
-                <h1 className="text-sm opacity-50">© 2023 Sleek. All Rights Reserved.</h1>
-            </div>
-            <div className="flex justify-between gap-24">
-                <div className="flex flex-col gap-4">
-                    <h1 className="text-xl">Product</h1>
-                    <h1 className="text-base">Home</h1>
-                    <h1 className="text-base">Features</h1>
-                    <h1 className="text-base">Pricing</h1>
-                    <h1 className="text-base">Changelog</h1>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <h1 className="text-xl">Company</h1>
-                    <h1 className="text-base">About</h1>
-                    <h1 className="text-base">Contact</h1>
-                </div>
-                <div className="flex flex-col gap-4">
-                    <h1 className="text-xl">Compare</h1>
-                    <h1 className="text-base">Asana</h1>
-                    <h1 className="text-base">Trello</h1>
-                    <h1 className="text-base">Notion</h1>
-                </div>
-            </div>
-            <div className="flex basis-2/6 flex-col gap-5">
-                <h1 className="text-xl">Join Our Newsletter</h1>
-                <h1 className="text-base">Get new updates, insight, and an early promo for our product. Don’t worry we won’t spam you :)</h1>
-                <div className="relative flex flex-row gap-4">
-                    <input className="shrink-0 basis-2/3 rounded-lg bg-dark-500 py-5 pr-5 pl-10 focus:outline-none" type="text" placeholder="Enter Your Email Here" />
-                    <IconMail className="absolute top-1/3 left-2.5" />
-                    <Button.Small>Subscribe</Button.Small>
-                </div>
-            </div>
-        </div>
+        <footer className="w-full bg-dark-800">
+            <main className="flex max-w-screen-3xl justify-between bg-dark-700 px-48 py-14 text-dark-50">
+                <section className="flex w-72 shrink-0 flex-col gap-5">
+                    <img src="/logoipsum-286.svg" className="h-8 w-max text-white" alt="Logo" />
+
+                    <h4 className="ts-base">Sleek is the most flexible project management app for your team.</h4>
+
+                    <div className="flex gap-7">
+                        <IconBrandTwitter />
+                        <IconBrandDiscord />
+                    </div>
+
+                    <span className="ts-sm opacity-50">© 2023 Sleek. All Rights Reserved.</span>
+                </section>
+
+                <FooterSection
+                    title="Product"
+                    links={[
+                        { title: "Home", href: "/" },
+                        { title: "Features", href: "/features" },
+                        { title: "Pricing", href: "/pricing" },
+                        { title: "Changelog", href: "/changelog" },
+                    ]}
+                />
+
+                <FooterSection
+                    title="Company"
+                    links={[
+                        { title: "About", href: "/about" },
+                        { title: "Contact", href: "/contact" },
+                    ]}
+                />
+
+                <FooterSection
+                    title="Compare"
+                    links={[
+                        { title: "Asana", href: "/compare/asana" },
+                        { title: "Trello", href: "/compare/trello" },
+                        { title: "Notion", href: "/compare/notion" },
+                    ]}
+                />
+
+                <section className="flex max-w-lg flex-col gap-5">
+                    <h3 className="ts-xl">Join Our Newsletter</h3>
+                    <p className="ts-base">Get new updates, insight, and an early promo for our product. Don’t worry we won’t spam you :)</p>
+
+                    <div className="flex gap-4">
+                        <Input.Large placeholder="Email" icon={IconMail} />
+                        <Button.Large fit>Subscribe</Button.Large>
+                    </div>
+                </section>
+            </main>
+        </footer>
     );
 }
--- src/components/Sections/Navbar.tsx
+++ src/components/Sections/Navbar.tsx
@@ -1,30 +1,35 @@
 import Button from "@/components/Forms/Button";
 
-import { IconLogin } from "@tabler/icons";
+import { IconDoorEnter, IconLogin } from "@tabler/icons";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
-export default function Navbar() {
+export default function Navigation() {
+    const { data: session, status } = useSession();
+
     return (
-        <div className="bg-dark-800">
-            <div className="flex justify-between px-48 py-8 text-dark-50">
-                <div className="flex gap-12">
+        <nav className="w-full bg-dark-800">
+            <main className="ts-base mx-auto flex max-w-screen-3xl justify-between px-48 py-8 text-dark-50">
+                <div className="flex items-center gap-12">
                     <img src="/logoipsum-286.svg" className="text-white" alt="logo_ipsum" width="174" height="32" />
-                    <Link className="flex flex-col justify-center" href="/pricing">
-                        Pricing
-                    </Link>
-                    <Link className="flex flex-col justify-center" href="/contact">
-                        Contact
-                    </Link>
+
+                    <Link href="/pricing">Pricing</Link>
+
+                    <Link href="/contact">Contact</Link>
                 </div>
-                <div className="flex gap-5">
-                    <a className="flex flex-col justify-center" href="#">
-                        Log in
-                    </a>
-                    <div>
-                        <Button.Large icon={IconLogin}>Create an Account</Button.Large>
-                    </div>
+
+                <div className="flex items-center gap-5">
+                    {session ? (
+                        <Link href="/app">
+                            <Button.Large icon={IconDoorEnter}>Go to Dashboard</Button.Large>
+                        </Link>
+                    ) : (
+                        <Link href="/api/auth/signin">
+                            <Button.Large icon={IconLogin}>Log In / Sign In</Button.Large>
+                        </Link>
+                    )}
                 </div>
-            </div>
-        </div>
+            </main>
+        </nav>
     );
 }
