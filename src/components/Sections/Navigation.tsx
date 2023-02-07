4a2697c52a4e5d2975d1266ba57f08a675b94e49
Rafly Maulana <mraflymaulana@gmail.com>
2023-02-08 03:31:31 +0700

    Allow adding custom class to navigation component

--- src/components/Sections/Navigation.tsx
+++ src/components/Sections/Navigation.tsx
@@ -4,11 +4,15 @@ import { IconDoorEnter, IconLogin } from "@tabler/icons";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 
-export default function Navigation() {
+type NavigationProps = {
+    className?: string;
+};
+
+export default function Navigation({ className }: NavigationProps) {
     const { data: session } = useSession();
 
     return (
-        <nav className="w-full bg-dark-800 text-dark-50">
+        <nav className={`w-full bg-dark-800 text-dark-50 ${className}`}>
             <main className="ts-base mx-auto flex max-w-screen-3xl justify-between px-48 py-8">
                 <div className="flex items-center gap-12">
                     <img src="/logoipsum-286.svg" className="text-white" alt="logo_ipsum" width="174" height="32" />
@@ -21,7 +25,7 @@ export default function Navigation() {
                 <div className="flex items-center gap-5">
                     {session ? (
                         <Link href="/app">
-                            <Button.Large icon={IconDoorEnter}>Go to Dashboard</Button.Large>
+                            <Button.Large icon={IconDoorEnter}>Go to Application</Button.Large>
                         </Link>
                     ) : (
                         <Link href="/api/auth/signin">
