2fea9eb368ed19c247dfd0d1605ac86bd2350080
Rafly Maulana <mraflymaulana@gmail.com>
2023-02-14 18:35:36 +0700

    Split introduction section to different component + add logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,116 @@
+import type { TablerIcon } from "@tabler/icons";
+
 import Button from "@/components/Forms/Button";
 import Input from "@/components/Forms/Input";
+import SwitchButton from "@/components/Forms/SwitchButton";
 import Container from "@/components/Sections/Container";
 import FAQ from "@/components/Sections/FAQ";
 import Footer from "@/components/Sections/Footer";
 import Navigation from "@/components/Sections/Navigation";
 
-import { IconDoorEnter, IconFolder, IconLayout, IconMail, IconReload, IconShare } from "@tabler/icons";
+import { IconDoorEnter, IconFolder, IconLayout, IconMail, IconReload, IconSettings } from "@tabler/icons";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+
+type IntroductionsContextType = {
+    activeIndex: number;
+    setActiveIndex: (index: number) => void;
+    pictureUrl: string;
+    setPictureUrl: (url: string) => void;
+};
+
+const IntroductionsContext = createContext<IntroductionsContextType>(null as unknown as IntroductionsContextType);
+
+type IntroductionProps = {
+    icon: TablerIcon;
+    title: string;
+    description: string;
+    pictureUrl: string;
+    buttons?: {
+        title: string;
+        pictureUrl: string;
+    }[];
+};
+
+const Introduction = ({ icon: Icon, title, description, pictureUrl, buttons, index }: IntroductionProps & { index: number }) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const { pictureUrl: currentPictureUrl, setPictureUrl, activeIndex, setActiveIndex } = useContext(IntroductionsContext);
+
+    useEffect(() => {
+        if (!ref.current) return;
+
+        // check if the element is in the viewport and is on the middle of the screen, if so, set the active index
+        const check = () => {
+            if (!ref.current) return;
+
+            const rect = ref.current.getBoundingClientRect();
+
+            if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2 && activeIndex !== index) {
+                setActiveIndex(index);
+                setPictureUrl(pictureUrl);
+                console.log("set");
+            }
+        };
+
+        document.addEventListener("scroll", check);
+
+        return () => {
+            document.removeEventListener("scroll", check);
+        };
+    }, [activeIndex, currentPictureUrl, index, pictureUrl, setActiveIndex, setPictureUrl]);
+
+    return (
+        <section ref={ref} className="flex w-full gap-20 lg:gap-0">
+            <div className="flex flex-1 flex-col justify-center gap-5">
+                <div className="w-max rounded-xl border border-dark-500 bg-dark-600 p-3">
+                    <Icon width={32} height={32} />
+                </div>
+
+                <h4 className="heading-4">{title}</h4>
+                <p className="ts-xl">{description}</p>
+
+                {buttons && (
+                    <div className="flex w-full flex-wrap gap-4">
+                        {buttons.map(({ title, pictureUrl }, index) => (
+                            <SwitchButton key={index} onClick={() => setPictureUrl(pictureUrl)} active={pictureUrl === currentPictureUrl}>
+                                {title}
+                            </SwitchButton>
+                        ))}
+                    </div>
+                )}
+            </div>
+
+            <img src={pictureUrl} alt="" loading="lazy" className="h-80 w-full shrink-0 rounded-3xl bg-dark-600 lg:w-0" />
+        </section>
+    );
+};
+
+const IntroductionList = ({ data }: { data: IntroductionProps[] }) => {
+    const [pictureUrl, setPictureUrl] = useState(data[0].pictureUrl);
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    return (
+        <Container className={["bg-gradient-to-b from-dark-700 to-dark-800", "flex gap-20 px-80 py-20"]}>
+            <IntroductionsContext.Provider
+                value={{
+                    pictureUrl,
+                    setPictureUrl,
+                    activeIndex,
+                    setActiveIndex,
+                }}
+            >
+                <section className="flex flex-1 flex-col gap-20">
+                    {data.map((props, index) => (
+                        <Introduction key={index} index={index} {...props} />
+                    ))}
+                </section>
+
+                <section className="hidden w-full max-w-xl shrink-0 lg:block">
+                    <img src={pictureUrl} alt="" loading="lazy" className="sticky top-1/3 h-80 w-full rounded-3xl bg-dark-600" />
+                </section>
+            </IntroductionsContext.Provider>
+        </Container>
+    );
+};
 
 export default function HomePage() {
     const features = [
@@ -23,26 +128,48 @@ export default function HomePage() {
         },
     ];
 
-    const introductions = [
+    const introductions: IntroductionProps[] = [
         {
             icon: IconLayout,
             title: "View the data in the way you want.",
             description: "Switch between views easily, you can choose whether it’s Kanban, Table, Timeline, or even Date view.",
+            pictureUrl: "https://picsum.photos/id/100/500/300",
+            buttons: [
+                {
+                    title: "Kanban View",
+                    pictureUrl: "https://picsum.photos/id/100/500/300",
+                },
+                {
+                    title: "Table View",
+                    pictureUrl: "https://picsum.photos/id/200/500/300",
+                },
+                {
+                    title: "Timeline View",
+                    pictureUrl: "https://picsum.photos/id/300/500/300",
+                },
+                {
+                    title: "Date View",
+                    pictureUrl: "https://picsum.photos/id/400/500/300",
+                },
+            ],
         },
         {
             icon: IconReload,
             title: "Sync your update with your coworker.",
             description: "Your coworker will see what you doing in real time, so don’t worry about that.",
+            pictureUrl: "https://picsum.photos/id/234/500/300",
         },
         {
             icon: IconFolder,
             title: "Make it clean.",
             description: "Put that, put this in this folder, and so on, organize the work the way you want.",
+            pictureUrl: "https://picsum.photos/id/345/500/300",
         },
         {
-            icon: IconShare,
+            icon: IconSettings,
             title: "Doesn’t like the default customization?",
             description: "Simply change or customize, you get access to every feature we provide so you can make your job easier.",
+            pictureUrl: "https://picsum.photos/id/456/500/300",
         },
     ];
 
@@ -142,24 +269,7 @@ export default function HomePage() {
                 <div className="absolute bottom-0 left-0 z-10 h-1/4 w-full rounded-t-3xl bg-dark-700" />
             </Container>
 
-            <Container className={["bg-gradient-to-b from-dark-700 to-dark-800", "flex gap-20 px-80 py-20"]}>
-                <section className="flex flex-1 flex-col gap-80">
-                    {introductions.map(({ icon: Icon, title, description }, index) => (
-                        <article key={index} className="flex flex-col gap-5">
-                            <div className="w-max rounded-xl border border-dark-500 bg-dark-600 p-5">
-                                <Icon width={32} height={32} />
-                            </div>
-
-                            <h4 className="heading-4">{title}</h4>
-                            <p className="ts-xl">{description}</p>
-                        </article>
-                    ))}
-                </section>
-
-                <section className="w-full max-w-xl shrink-0">
-                    <div className="sticky top-1/3 h-80 w-full rounded-3xl bg-dark-600" />
-                </section>
-            </Container>
+            <IntroductionList data={introductions} />
 
             <Container className={["bg-dark-800", "flex flex-col items-center gap-14 px-80 py-20"]}>
                 <h3 className="heading-3">What Peoples Say</h3>
