289a7036ea145154d6a3d0506b75d66a7ffa43ac
Rafly Maulana <mraflymaulana@gmail.com>
2023-02-17 23:57:53 +0700

    Memoize SwitchButton before exporting

--- src/components/Forms/SwitchButton.tsx
+++ src/components/Forms/SwitchButton.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { memo } from "react";
 
 export type SwitchButtonProps = {
     active?: boolean;
@@ -6,10 +7,12 @@ export type SwitchButtonProps = {
     onClick?: () => void;
 };
 
-export default function SwitchButton({ active = false, onClick, children }: SwitchButtonProps) {
+const SwitchButton = ({ active = false, onClick, children }: SwitchButtonProps) => {
     return (
         <button onClick={onClick} className={clsx("ts-sm border-b-2 px-4 py-3 duration-200 hover:border-b-dark-500", active ? "border-b-dark-400" : "border-b-transparent")}>
             {children}
         </button>
     );
-}
+};
+
+export default memo(SwitchButton);
