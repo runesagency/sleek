d6d3def787b0b6d46ce10061dac4c825e3ad3346
Rafly Maulana <mraflymaulana@gmail.com>
2023-02-17 21:29:21 +0700

    Update formula to show menu based on alignment and direction

--- src/lib/menu/components/Menu.tsx
+++ src/lib/menu/components/Menu.tsx
@@ -73,35 +73,244 @@ const Menu = () => {
                 cursorOffsetY = clientY - posY;
             }
 
-            if (!colliderOffsetX && posX + cursorOffsetX + menuElement.offsetWidth > windowWidth) {
-                if (targetPosition.current === MenuPosition.Cursor) {
-                    colliderOffsetX = posX + cursorOffsetX + menuElement.offsetWidth - windowWidth;
-                } else {
-                    colliderOffsetX = posX + width + menuElement.offsetWidth - windowWidth;
+            let usedX = 0;
+            let usedY = 0;
+
+            if (anchor.current === MenuAnchor.Cursor) {
+                const locX = posX + cursorOffsetX;
+                const locY = posY + cursorOffsetY;
+
+                switch (direction.current) {
+                    // Horizontal
+                    case MenuDirection.Right:
+                    case MenuDirection.Left: {
+                        if (alignment.current === MenuAlignment.Start) {
+                            if (!colliderOffsetY && locY + menuElement.offsetHeight > windowHeight) {
+                                colliderOffsetY = locY + menuElement.offsetHeight - windowHeight;
+                            }
+
+                            usedY = locY - offsetY - colliderOffsetY;
+                        } else if (alignment.current === MenuAlignment.Center) {
+                            const pos = locY - offsetY - menuElement.offsetHeight / 2;
+
+                            if (!colliderOffsetY) {
+                                if (locY + menuElement.offsetHeight > windowHeight) {
+                                    colliderOffsetY = locY + menuElement.offsetHeight / 2 - windowHeight;
+                                }
+
+                                if (pos < 0) {
+                                    colliderOffsetY = pos;
+                                }
+                            }
+
+                            usedY = pos - colliderOffsetY;
+                        } else if (alignment.current === MenuAlignment.End) {
+                            const pos = locY - offsetY - menuElement.offsetHeight;
+
+                            if (!colliderOffsetY && pos < 0) {
+                                colliderOffsetY = pos;
+                            }
+
+                            usedY = pos - colliderOffsetY;
+                        }
+
+                        if (direction.current === MenuDirection.Right) {
+                            const pos = locX - offsetX;
+
+                            if (!colliderOffsetX && pos + menuElement.offsetWidth > windowWidth) {
+                                colliderOffsetX = pos + menuElement.offsetWidth - windowWidth;
+                            }
+
+                            usedX = pos - colliderOffsetX;
+                        } else if (direction.current === MenuDirection.Left) {
+                            const pos = locX - offsetX - menuElement.offsetWidth;
+
+                            if (!colliderOffsetX && pos < 0) {
+                                colliderOffsetX = pos;
+                            }
+
+                            usedX = pos - colliderOffsetX;
+                        }
+
+                        break;
+                    }
+
+                    // Vertical
+                    case MenuDirection.Top:
+                    case MenuDirection.Bottom: {
+                        if (alignment.current === MenuAlignment.Start) {
+                            // No need for the collider to the left since the menu will
+                            // be aligned to the left to begin with (to right of the cursor)
+                            if (!colliderOffsetX && locX + menuElement.offsetWidth > windowWidth) {
+                                colliderOffsetX = locX + menuElement.offsetWidth - windowWidth;
+                            }
+
+                            usedX = locX - colliderOffsetX;
+                        } else if (alignment.current === MenuAlignment.Center) {
+                            const pos = locX - menuElement.offsetWidth / 2;
+
+                            // Define the collider to the left if the menu is out of the viewport
+                            if (!colliderOffsetX) {
+                                // If the menu is out of the viewport to the right
+                                if (locX + menuElement.offsetWidth > windowWidth) {
+                                    colliderOffsetX = locX + menuElement.offsetWidth / 2 - windowWidth;
+                                }
+
+                                // If the menu is out of the viewport to the left
+                                if (pos < 0) {
+                                    colliderOffsetX = pos;
+                                }
+                            }
+
+                            usedX = pos - colliderOffsetX;
+                        } else if (alignment.current === MenuAlignment.End) {
+                            const pos = locX - menuElement.offsetWidth;
+
+                            // No need for the collider to the right since the menu will
+                            // be aligned to the right to begin with (to left of the cursor)
+                            if (!colliderOffsetX && pos < 0) {
+                                colliderOffsetX = pos;
+                            }
+
+                            usedX = pos - colliderOffsetX;
+                        }
+
+                        if (direction.current === MenuDirection.Top) {
+                            const pos = locY - menuElement.offsetHeight;
+
+                            // If the menu is out of the viewport to the top,
+                            // set the menu's position to the top of the viewport
+                            if (!colliderOffsetY && pos < 0) {
+                                colliderOffsetY = pos;
+                            }
+
+                            usedY = pos - colliderOffsetY;
+                        } else if (direction.current === MenuDirection.Bottom) {
+                            const pos = locY + offsetY;
+
+                            // If the menu is out of the viewport to the bottom,
+                            // set the menu's position to the bottom of the viewport
+                            if (!colliderOffsetY && pos + menuElement.offsetHeight > windowHeight) {
+                                colliderOffsetY = pos - windowHeight - menuElement.offsetHeight;
+                            }
+
+                            usedY = pos - colliderOffsetY;
+                        }
+
+                        break;
+                    }
+                }
+            } else {
+                switch (direction.current) {
+                    case MenuDirection.Right:
+                    case MenuDirection.Left: {
+                        if (alignment.current === MenuAlignment.Start) {
+                            if (!colliderOffsetY && posY + menuElement.offsetHeight > windowHeight) {
+                                colliderOffsetY = posY + menuElement.offsetHeight - windowHeight;
+                            }
+
+                            usedY = posY - colliderOffsetY;
+                        } else if (alignment.current === MenuAlignment.Center) {
+                            const pos = posY + height / 2 - menuElement.offsetHeight / 2;
+
+                            if (!colliderOffsetY) {
+                                if (posY + menuElement.offsetHeight > windowHeight) {
+                                    colliderOffsetY = posY + menuElement.offsetHeight / 2 - windowHeight;
+                                }
+
+                                if (pos < 0) {
+                                    colliderOffsetY = pos;
+                                }
+                            }
+
+                            usedY = pos - colliderOffsetY;
+                        } else if (alignment.current === MenuAlignment.End) {
+                            const pos = posY + height - menuElement.offsetHeight;
+
+                            if (!colliderOffsetY && pos < 0) {
+                                colliderOffsetY = pos;
+                            }
+
+                            usedY = pos - colliderOffsetY;
+                        }
+
+                        if (direction.current === MenuDirection.Right) {
+                            const pos = posX + width;
+
+                            if (!colliderOffsetX && pos + menuElement.offsetWidth > windowWidth) {
+                                colliderOffsetX = pos + menuElement.offsetWidth - windowWidth;
+                            }
+
+                            usedX = pos - colliderOffsetX;
+                        } else if (direction.current === MenuDirection.Left) {
+                            const pos = posX - menuElement.offsetWidth;
+
+                            if (!colliderOffsetX && pos < 0) {
+                                colliderOffsetX = pos;
+                            }
+
+                            usedX = pos - colliderOffsetX;
+                        }
+
+                        break;
+                    }
+
+                    case MenuDirection.Top:
+                    case MenuDirection.Bottom: {
+                        if (alignment.current === MenuAlignment.Start) {
+                            if (!colliderOffsetX && posX + menuElement.offsetWidth > windowWidth) {
+                                colliderOffsetX = posX + menuElement.offsetWidth - windowWidth;
+                            }
+
+                            usedX = posX - colliderOffsetX;
+                        } else if (alignment.current === MenuAlignment.Center) {
+                            const pos = posX + width / 2 - menuElement.offsetWidth / 2;
+
+                            if (!colliderOffsetX) {
+                                if (posX + menuElement.offsetWidth > windowWidth) {
+                                    colliderOffsetX = posX + menuElement.offsetWidth / 2 - windowWidth;
+                                }
+
+                                if (pos < 0) {
+                                    colliderOffsetX = pos;
+                                }
+                            }
+
+                            usedX = pos - colliderOffsetX;
+                        } else if (alignment.current === MenuAlignment.End) {
+                            const pos = posX + width - menuElement.offsetWidth;
+
+                            if (!colliderOffsetX && pos < 0) {
+                                colliderOffsetX = pos;
+                            }
+
+                            usedX = pos - colliderOffsetX;
+                        }
+
+                        if (direction.current === MenuDirection.Top) {
+                            const pos = posY - menuElement.offsetHeight;
+
+                            if (!colliderOffsetY && pos < 0) {
+                                colliderOffsetY = pos;
+                            }
+
+                            usedY = pos - colliderOffsetY;
+                        } else if (direction.current === MenuDirection.Bottom) {
+                            const pos = posY + height;
+
+                            if (!colliderOffsetY && pos + menuElement.offsetHeight > windowHeight) {
+                                colliderOffsetY = pos - windowHeight - menuElement.offsetHeight;
+                            }
+
+                            usedY = pos - colliderOffsetY;
+                        }
+                    }
                 }
             }
-
-            if (!colliderOffsetY && posY + cursorOffsetY + menuElement.offsetHeight > windowHeight) {
-                if (targetPosition.current === MenuPosition.Cursor) {
-                    colliderOffsetY = posY + cursorOffsetY + menuElement.offsetHeight - windowHeight;
-                } else {
-                    colliderOffsetY = posY + height + menuElement.offsetHeight - windowHeight;
-                }
-            }
-
-            let usedX =
-                targetPosition.current === MenuPosition.Cursor //
-                    ? posX + cursorOffsetX
-                    : posX + width;
-
-            let usedY =
-                targetPosition.current === MenuPosition.Cursor //
-                    ? posY + cursorOffsetY
-                    : posY;
 
             setCoordinates({
-                x: usedX - colliderOffsetX + offsetX,
-                y: usedY - colliderOffsetY + offsetY,
+                x: usedX + offsetX,
+                y: usedY + offsetY,
             });
         };
 
