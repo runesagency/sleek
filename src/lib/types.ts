9fa79e178e8c0b1b283f850aa7f38268fa6eba3d
Rafly Maulana <mraflymaulana@gmail.com>
2023-02-25 22:04:44 +0700

    Update API Type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,29 +2,23 @@ import type { User } from "@prisma/client";
 import type { Server as NetServer, Socket } from "net";
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { Server as SocketIOServer } from "socket.io";
-import type { ZodError } from "zod";
-
-export type APIMethodFunction<BodySchema, Result> = (user: User, body: BodySchema) => Promise<APIMethodResult<Result, BodySchema>>;
-
-export type APIMethodResult<Result, SchemaType = unknown> =
-    | {
-          result: Result;
-          error?: never;
-      }
-    | {
-          result?: never;
-          error: ZodError<SchemaType>;
-      };
 
 export type ApiRequest = NextApiRequest & {
     params: Record<string, string>;
     user: User;
 };
 
-export type APIResponse = NextApiResponse<{
-    result?: unknown;
-    error?: unknown;
-}> & {
+export type APIResult<Result = unknown> =
+    | {
+          result: Result;
+          error?: never;
+      }
+    | {
+          result?: never;
+          error: Error;
+      };
+
+export type APIResponse<Result = unknown> = NextApiResponse<APIResult<Result>> & {
     socket: Socket & {
         server: NetServer & {
             io: SocketIOServer;
