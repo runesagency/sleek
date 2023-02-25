4da3ab0dbd8efa9c0fbff636cb5df49dadf44823
Rafly Maulana <mraflymaulana@gmail.com>
2023-02-25 22:05:12 +0700

    Code fix

--- src/pages/app/organization/[id]/index.tsx
+++ src/pages/app/organization/[id]/index.tsx
@@ -1,7 +1,5 @@
 import Project from "@/components/App/DataDisplay/Project";
 import OrganizationPageLayout from "@/components/App/Layout/OrganizationPageLayout";
-import Members from "@/pages/app/organization/[id]/members";
-import Settings from "@/pages/app/organization/[id]/settings";
 
 import { IconFolder } from "@tabler/icons";
 
@@ -9,18 +7,17 @@ export default function OrganizationProjectListPage() {
     return (
         <OrganizationPageLayout className="flex flex-col gap-10">
             {/* Folders (Future Development) */}
-            {/* <div className="flex flex-wrap gap-5">
+            <div className="flex flex-wrap gap-5">
                 <button className="flex items-center gap-4 truncate rounded-lg border border-dark-500 bg-dark-600 px-5 py-4">
                     <IconFolder height={20} width={undefined} />
                     <span>Folder Name</span>
                 </button>
-            </div> */}
-            <Members />
-            {/* <Settings/> */}
+            </div>
+
             {/* Project List */}
-            {/* <div className="grid grid-cols-3 gap-5">
+            <div className="grid grid-cols-3 gap-5">
                 <Project />
-            </div> */}
+            </div>
         </OrganizationPageLayout>
     );
 }
