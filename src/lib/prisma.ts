92c4f7d8963048040242d872d8e15efd685b2654
Rafly Maulana <mraflymaulana@gmail.com>
2023-02-27 23:01:22 +0700

    Prevent any schema for the database other than `dev` on development mode

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined");
+}
+
+const parsedDatabaseUrl = new URL(process.env.DATABASE_URL);
+const databaseSchema = parsedDatabaseUrl.searchParams.get("schema");
+
+if ((!databaseSchema || databaseSchema !== "dev") && process.env.NODE_ENV !== "production") {
+    throw new Error("Please only use the development (dev) schema on the database on development mode");
+}
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
