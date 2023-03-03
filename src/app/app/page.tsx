e4772d68851ba2714a387082cef692d71a4d8422
Rafly Maulana <mraflymaulana@gmail.com>
2023-03-03 22:13:01 +0700

    Update app page
    - Change loading animation
    - Add empty/skeleton element to fill base design (e.g. empty project shell)

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -23,40 +23,46 @@ export default function AppUserHomePage() {
                 ? [...Array(5)].map((_, index) => (
                       <section key={index} className="flex flex-col gap-5">
                           <div className="flex flex-col gap-3">
-                              <div className="h-8 w-40 animate-pulse rounded-lg bg-dark-700" />
-                              <div className="h-5 w-20 animate-pulse rounded-lg bg-dark-700" />
+                              <div className="animate-shimmer h-8 w-40 rounded-lg bg-dark-700" />
+                              <div className="animate-shimmer h-5 w-20 rounded-lg bg-dark-700" />
                           </div>
 
                           <div className="grid gap-5 md:grid-cols-2 xl:grid-cols-3">
                               {[...Array(3)].map((_, index) => (
-                                  <div key={index} className="h-40 w-full animate-pulse rounded-lg bg-dark-700" />
+                                  <div key={index} className="animate-shimmer h-40 w-full rounded-lg bg-dark-700" />
                               ))}
                           </div>
                       </section>
                   ))
-                : organizations.map(({ id, name, description, users, projects }, index) => (
-                      <section key={index} className="flex flex-col gap-5">
-                          <div className="flex flex-col gap-3">
-                              <h2 className="ts-2xl">{name}</h2>
+                : organizations.map(({ id, name, description, users, projects }, index) => {
+                      const shadowProjectCount = Math.max(0, 3 - projects.length);
 
-                              <p className="ts-sm">{description || "No Description Available"}</p>
+                      return (
+                          <section key={index} className="flex flex-col gap-5">
+                              <div className="flex flex-col gap-3">
+                                  <h2 className="ts-2xl">{name}</h2>
 
-                              <MemberList.Large users={users} min={5} />
+                                  <p className="ts-sm">{description || "No Description Available"}</p>
 
-                              <Link href={`/app/organization/${id}`} className="w-max">
-                                  <Button.Large fit>See More</Button.Large>
-                              </Link>
-                          </div>
+                                  <MemberList.Large users={users} min={5} />
 
-                          {projects.length > 0 && (
+                                  <Link href={`/app/organization/${id}`} className="w-max">
+                                      <Button.Large fit>See More</Button.Large>
+                                  </Link>
+                              </div>
+
                               <div className="grid gap-5 md:grid-cols-2 xl:grid-cols-3">
                                   {projects.map((project, index) => (
                                       <Project key={index} {...project} />
                                   ))}
+
+                                  {[...Array(shadowProjectCount)].map((_, index) => (
+                                      <div key={index} className="ts-base flex h-32 w-full items-center justify-center rounded-lg bg-dark-800 opacity-75" />
+                                  ))}
                               </div>
-                          )}
-                      </section>
-                  ))}
+                          </section>
+                      );
+                  })}
         </main>
     );
 }
