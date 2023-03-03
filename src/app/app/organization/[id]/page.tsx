c38f8963b996bf42e6b262c3165816b9cfdaa7d8
Rafly Maulana <mraflymaulana@gmail.com>
2023-03-03 22:13:25 +0700

    Disable folder since the feature still not available yet

--- src/app/app/organization/[id]/page.tsx
+++ src/app/app/organization/[id]/page.tsx
@@ -14,12 +14,14 @@ export default function OrganizationProjectListPage() {
     return (
         <main className="flex flex-col gap-10">
             {/* Folders (Future Development) */}
-            <div className="flex flex-wrap gap-5">
-                <button className="flex items-center gap-4 truncate rounded-lg border border-dark-500 bg-dark-600 px-5 py-4">
-                    <IconFolder height={20} width={undefined} />
-                    <span>Folder Name</span>
-                </button>
-            </div>
+            {/* {!isLoading && (
+                <div className="flex flex-wrap gap-5">
+                    <button className="flex items-center gap-4 truncate rounded-lg border border-dark-500 bg-dark-600 px-5 py-4">
+                        <IconFolder height={20} width={undefined} />
+                        <span>Folder Name</span>
+                    </button>
+                </div>
+            )} */}
 
             {/* Project List */}
             <div className="grid grid-cols-3 gap-5">
