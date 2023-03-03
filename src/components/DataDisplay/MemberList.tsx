ef11b213968e9e6e76c38ad5654ddbd9e0c2a94b
Rafly Maulana <mraflymaulana@gmail.com>
2023-03-03 14:48:47 +0700

    Add min props to add shadow avatar if user length is less

--- src/components/DataDisplay/MemberList.tsx
+++ src/components/DataDisplay/MemberList.tsx
@@ -6,9 +6,19 @@ type MemberListProps = {
     users: User[];
     max?: number;
     className?: string;
+    /**
+     * When using min, if the number of users is less than min, it will be padded with empty avatars.
+     */
+    min?: number;
 };
 
-const Small = ({ users, max }: MemberListProps) => {
+const Small = ({ users, max, min }: MemberListProps) => {
+    let shadowAvatarCount = 0;
+
+    if (min && users.length < min) {
+        shadowAvatarCount = min - users.length;
+    }
+
     return (
         <div className="flex shrink-0 -space-x-1">
             {users.map((user, i) => {
@@ -18,11 +28,21 @@ const Small = ({ users, max }: MemberListProps) => {
 
                 return <Avatar key={i} className="h-6 w-6 rounded-full border border-dark-600 object-cover object-center" seed={title} />;
             })}
+
+            {[...Array(shadowAvatarCount)].map((_, i) => (
+                <div key={i} className="h-6 w-6 rounded-full border border-dark-600 bg-dark-700 object-cover object-center" />
+            ))}
         </div>
     );
 };
 
-const Large = ({ users, max }: MemberListProps) => {
+const Large = ({ users, max, min }: MemberListProps) => {
+    let shadowAvatarCount = 0;
+
+    if (min && users.length < min) {
+        shadowAvatarCount = min - users.length;
+    }
+
     return (
         <div className="flex shrink-0 -space-x-2">
             {users.map((user, i) => {
@@ -32,6 +52,10 @@ const Large = ({ users, max }: MemberListProps) => {
 
                 return <Avatar key={i} className="h-10 w-10 rounded-full border border-dark-600 object-cover object-center" seed={title} />;
             })}
+
+            {[...Array(shadowAvatarCount)].map((_, i) => (
+                <div key={i} className="h-10 w-10 rounded-full border border-dark-600 bg-dark-700 object-cover object-center" />
+            ))}
         </div>
     );
 };
