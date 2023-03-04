2d4797b9f94388df461c618d9571acd01445a76b
Rafly Maulana <mraflymaulana@gmail.com>
2023-03-04 10:49:38 +0700

    Change skeleton when no project available

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -34,35 +34,29 @@ export default function AppUserHomePage() {
                           </div>
                       </section>
                   ))
-                : organizations.map(({ id, name, description, users, projects }, index) => {
-                      const shadowProjectCount = Math.max(0, 3 - projects.length);
+                : organizations.map(({ id, name, description, users, projects }, index) => (
+                      <section key={index} className="flex flex-col gap-5">
+                          <div className="flex flex-col gap-3">
+                              <h2 className="ts-2xl">{name}</h2>
 
-                      return (
-                          <section key={index} className="flex flex-col gap-5">
-                              <div className="flex flex-col gap-3">
-                                  <h2 className="ts-2xl">{name}</h2>
+                              <p className="ts-sm">{description || "No Description Available"}</p>
 
-                                  <p className="ts-sm">{description || "No Description Available"}</p>
+                              <MemberList.Large users={users} min={5} />
 
-                                  <MemberList.Large users={users} min={5} />
+                              <Link href={`/app/organization/${id}`} className="w-max">
+                                  <Button.Large fit>See More</Button.Large>
+                              </Link>
+                          </div>
 
-                                  <Link href={`/app/organization/${id}`} className="w-max">
-                                      <Button.Large fit>See More</Button.Large>
-                                  </Link>
-                              </div>
-
-                              <div className="grid gap-5 md:grid-cols-2 xl:grid-cols-3">
-                                  {projects.map((project, index) => (
-                                      <Project key={index} {...project} />
-                                  ))}
-
-                                  {[...Array(shadowProjectCount)].map((_, index) => (
-                                      <div key={index} className="ts-base flex h-32 w-full items-center justify-center rounded-lg bg-dark-800 opacity-75" />
-                                  ))}
-                              </div>
-                          </section>
-                      );
-                  })}
+                          <div className="grid gap-5 md:grid-cols-2 xl:grid-cols-3">
+                              {projects.length === 0 ? (
+                                  <div className="ts-base flex h-32 w-full items-center justify-center rounded-lg bg-dark-800 opacity-75">No Projects Available</div>
+                              ) : (
+                                  projects.map((project, index) => <Project key={index} {...project} />)
+                              )}
+                          </div>
+                      </section>
+                  ))}
         </main>
     );
 }
