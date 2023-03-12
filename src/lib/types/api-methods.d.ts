cbc585b4a1bdfbab847dd50a73a0b80974c68845
Rafly Maulana <mraflymaulana@gmail.com>
2023-03-12 22:37:51 +0700

    Update api-methods.d.ts

--- src/lib/types/api-methods.d.ts
+++ src/lib/types/api-methods.d.ts
@@ -1,4 +1,5 @@
 export type * as CurrentUser from "@/pages/api/@me";
 export type * as Organizations from "@/pages/api/organizations";
 export type * as Organization from "@/pages/api/organizations/[id]";
-export type * as Project from "@/pages/api/projects";
+export type * as Projects from "@/pages/api/projects";
+export type * as Project from "@/pages/api/projects/[id]";
