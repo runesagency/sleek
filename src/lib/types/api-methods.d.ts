e7fc6d0f230ba2057949b95d375692646fdd694b
Rafly Maulana <mraflymaulana@gmail.com>
2023-03-13 14:35:50 +0700

    Add single board API method

--- src/lib/types/api-methods.d.ts
+++ src/lib/types/api-methods.d.ts
@@ -4,3 +4,4 @@ export type * as Organization from "@/pages/api/organizations/[id]";
 export type * as Projects from "@/pages/api/projects";
 export type * as Project from "@/pages/api/projects/[id]";
 export type * as Boards from "@/pages/api/boards";
+export type * as Board from "@/pages/api/boards/[id]";
