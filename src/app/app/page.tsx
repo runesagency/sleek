c652417d202a979f908ab4a95655ab12fcca4394
Rafly Maulana <mraflymaulana@gmail.com>
2023-03-21 23:01:23 +0700

    Change skeleton loading design

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -17,20 +17,7 @@ export default function AppUserHomePage() {
     return (
         <main ref={autoAnimateRef} className="flex flex-col gap-10 p-10 md:px-20">
             {isLoading
-                ? [...Array(5)].map((_, index) => (
-                      <section key={index} className="flex flex-col gap-5">
-                          <div className="flex flex-col gap-3">
-                              <div className="animate-shimmer h-8 w-40 rounded-lg bg-dark-700" />
-                              <div className="animate-shimmer h-5 w-20 rounded-lg bg-dark-700" />
-                          </div>
-
-                          <div className="grid gap-5 md:grid-cols-2 xl:grid-cols-3">
-                              {[...Array(3)].map((_, index) => (
-                                  <div key={index} className="animate-shimmer h-40 w-full rounded-lg bg-dark-700" />
-                              ))}
-                          </div>
-                      </section>
-                  ))
+                ? [...Array(5)].map((_, index) => <div key={index} className="animate-shimmer h-72 w-full rounded-lg bg-dark-800" />)
                 : data.map(({ id, name, description, users }, index) => (
                       <section key={index} className="flex flex-col gap-12 rounded-lg border border-dark-600 bg-dark-800 p-10">
                           <div className="flex flex-col gap-4">
