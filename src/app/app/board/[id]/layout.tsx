73d2c4d1e369926493f53f71b949f55c4ef5a06c
Rafly Maulana <mraflymaulana@gmail.com>
2023-03-22 01:14:53 +0700

    Split the layout for board view

--- src/app/app/board/[id]/layout.tsx
+++ src/app/app/board/[id]/layout.tsx
@@ -1,21 +1,16 @@
 "use client";
 
 import type { BoardCard, BoardList } from "@/app/app/board/[id]/BoardLayoutContext";
-import type { MenuOptions } from "@/lib/menu/hooks/use-menu";
 import type { ApiMethod, ApiResult } from "@/lib/types";
 
 import { defaultBoardLayoutContextValue, BoardLayoutContext } from "@/app/app/board/[id]/BoardLayoutContext";
-import MemberList from "@/components/DataDisplay/MemberList";
-import { SwitchButton, Button } from "@/components/Forms";
-import TaskModal from "@/components/TaskModal";
+import { SwitchButton } from "@/components/Forms";
 import { ApiRoutes, Routes } from "@/lib/constants";
 import { useRequest } from "@/lib/hooks";
-import { MenuAlignment, MenuAnchor, MenuDirection, MenuFormVariant, MenuVariant, useMenu } from "@/lib/menu";
 
-import { IconArrowBackUp, IconFilter, IconPlus } from "@tabler/icons";
 import Link from "next/link";
 import { useRouter, useSelectedLayoutSegment } from "next/navigation";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 import { toast } from "react-toastify";
 
 type BoardPageLayoutProps = {
@@ -27,11 +22,10 @@ type BoardPageLayoutProps = {
 
 export default function BoardPageLayout({ children, params: { id } }: BoardPageLayoutProps) {
     const { data, error, isLoading, mutate: setData } = useRequest<ApiMethod.Board.GetResult>(ApiRoutes.Board(id), defaultBoardLayoutContextValue.data);
-    const { openMenu, toggleMenu, openSubMenu } = useMenu();
     const router = useRouter();
     const currentSegment = useSelectedLayoutSegment();
 
-    const { projectId, name, users, lists, cards } = data;
+    const { lists, cards } = data;
     const [activeCard, setActiveCard] = useState<BoardCard | undefined>(undefined);
 
     const setLists = useCallback(
@@ -70,57 +64,6 @@ export default function BoardPageLayout({ children, params: { id } }: BoardPageL
             );
         },
         [setData]
-    );
-
-    const newListMenuOptions: MenuOptions = useMemo(() => {
-        return {
-            type: MenuVariant.Forms,
-            title: "Create New List",
-            submitButtonLabel: "Create",
-            lists: [
-                {
-                    id: "title",
-                    label: "Title",
-                    type: MenuFormVariant.Input,
-                    props: {
-                        autoFocus: true,
-                    },
-                },
-            ],
-            onSubmit: async ({ title }: { title: string }) => {
-                const res = await fetch(ApiRoutes.List, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        title,
-                        order: lists.length,
-                        boardId: id,
-                    }),
-                });
-
-                const { result, error }: ApiResult<ApiMethod.List.PostResult> = await res.json();
-
-                if (error) {
-                    return toast.error(error.message);
-                }
-
-                setLists([...lists, result]);
-            },
-        };
-    }, [id, lists, setLists]);
-
-    const onCreateNewList = useCallback(
-        (e: React.MouseEvent) => {
-            toggleMenu(e, {
-                alignment: MenuAlignment.Center,
-                direction: MenuDirection.Bottom,
-                anchor: MenuAnchor.Element,
-                ...newListMenuOptions,
-            });
-        },
-        [newListMenuOptions, toggleMenu]
     );
 
     const onCreateNewCard = useCallback(
@@ -179,29 +122,6 @@ export default function BoardPageLayout({ children, params: { id } }: BoardPageL
         [cards, id, lists, setCards]
     );
 
-    const onContextMenu = useCallback(
-        (e: React.MouseEvent) => {
-            openMenu(e, {
-                type: MenuVariant.Context,
-                anchor: MenuAnchor.Cursor,
-                direction: MenuDirection.Right,
-                lists: [
-                    {
-                        icon: IconPlus,
-                        name: "Create New List",
-                        onClick() {
-                            openSubMenu({
-                                ...newListMenuOptions,
-                                anchor: MenuAnchor.Cursor,
-                            });
-                        },
-                    },
-                ],
-            });
-        },
-        [newListMenuOptions, openMenu, openSubMenu]
-    );
-
     const links = [
         { name: "About", segment: "about" },
         { name: "View", segment: "view" },
@@ -216,7 +136,7 @@ export default function BoardPageLayout({ children, params: { id } }: BoardPageL
 
     return (
         <BoardLayoutContext.Provider value={{ isLoading, data, activeCard, refreshData: setData, setLists, setCards, setActiveCard, onCreateNewCard }}>
-            <main onContextMenu={onContextMenu} className="box-border flex h-full w-full flex-col">
+            <main className="flex h-full w-full flex-col">
                 <div className="flex bg-dark-700 px-11">
                     {links.map(({ name, segment }, index) => (
                         <Link key={index} href={Routes.Board(id) + "/" + (segment ?? "")}>
@@ -225,45 +145,7 @@ export default function BoardPageLayout({ children, params: { id } }: BoardPageL
                     ))}
                 </div>
 
-                <div className="flex items-center gap-6 px-11 py-6">
-                    {isLoading ? (
-                        <>
-                            <div className="animate-shimmer h-10 w-40 shrink-0 rounded-lg bg-dark-700" />
-                            <div className="animate-shimmer h-10 w-64 shrink-0 rounded-lg bg-dark-700" />
-                            <div className="animate-shimmer h-10 w-20 shrink-0 rounded-lg bg-dark-700" />
-                        </>
-                    ) : (
-                        <>
-                            <Link href={Routes.Project(projectId)}>
-                                <Button.Small icon={IconArrowBackUp} fit>
-                                    Back
-                                </Button.Small>
-                            </Link>
-
-                            <h2 className="ts-2xl">{name}</h2>
-
-                            <MemberList.Large users={users} />
-
-                            <Button.Large icon={IconFilter} fit>
-                                Filter
-                            </Button.Large>
-
-                            <Button.Large icon={IconPlus} fit onClick={onCreateNewList}>
-                                Create New List
-                            </Button.Large>
-                        </>
-                    )}
-                </div>
-
-                {isLoading ? (
-                    <div className="h-full w-full px-11 pb-6">
-                        <div className="animate-shimmer h-full w-full shrink-0 rounded-lg bg-dark-700" />
-                    </div>
-                ) : (
-                    children
-                )}
-
-                <TaskModal />
+                {children}
             </main>
         </BoardLayoutContext.Provider>
     );
---
+++ src/app/app/board/[id]/view/layout.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import type { MenuOptions } from "@/lib/menu/hooks/use-menu";
+import type { ApiMethod, ApiResult } from "@/lib/types";
+
+import { BoardLayoutContext } from "@/app/app/board/[id]/BoardLayoutContext";
+import MemberList from "@/components/DataDisplay/MemberList";
+import { Button } from "@/components/Forms";
+import TaskModal from "@/components/TaskModal";
+import { ApiRoutes, Routes } from "@/lib/constants";
+import { MenuAlignment, MenuAnchor, MenuDirection, MenuFormVariant, MenuVariant, useMenu } from "@/lib/menu";
+
+import { IconArrowBackUp, IconFilter, IconPlus } from "@tabler/icons";
+import Link from "next/link";
+import { useCallback, useContext, useMemo } from "react";
+import { toast } from "react-toastify";
+
+type BoardViewPageLayoutProps = {
+    children: React.ReactNode;
+};
+
+export default function BoardViewPageLayout({ children }: BoardViewPageLayoutProps) {
+    const {
+        data: { id: boardId, projectId, name, users, lists },
+        isLoading,
+        setLists,
+    } = useContext(BoardLayoutContext);
+
+    const { openMenu, toggleMenu, openSubMenu } = useMenu();
+
+    const newListMenuOptions: MenuOptions = useMemo(() => {
+        return {
+            type: MenuVariant.Forms,
+            title: "Create New List",
+            submitButtonLabel: "Create",
+            lists: [
+                {
+                    id: "title",
+                    label: "Title",
+                    type: MenuFormVariant.Input,
+                    props: {
+                        autoFocus: true,
+                    },
+                },
+            ],
+            onSubmit: async ({ title }: { title: string }) => {
+                const res = await fetch(ApiRoutes.List, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        title,
+                        order: lists.length,
+                        boardId,
+                    }),
+                });
+
+                const { result, error }: ApiResult<ApiMethod.List.PostResult> = await res.json();
+
+                if (error) {
+                    return toast.error(error.message);
+                }
+
+                setLists([...lists, result]);
+            },
+        };
+    }, [boardId, lists, setLists]);
+
+    const onCreateNewList = useCallback(
+        (e: React.MouseEvent) => {
+            toggleMenu(e, {
+                alignment: MenuAlignment.Center,
+                direction: MenuDirection.Bottom,
+                anchor: MenuAnchor.Element,
+                ...newListMenuOptions,
+            });
+        },
+        [newListMenuOptions, toggleMenu]
+    );
+
+    const onContextMenu = useCallback(
+        (e: React.MouseEvent) => {
+            openMenu(e, {
+                type: MenuVariant.Context,
+                anchor: MenuAnchor.Cursor,
+                direction: MenuDirection.Right,
+                lists: [
+                    {
+                        icon: IconPlus,
+                        name: "Create New List",
+                        onClick() {
+                            openSubMenu({
+                                ...newListMenuOptions,
+                                anchor: MenuAnchor.Cursor,
+                            });
+                        },
+                    },
+                ],
+            });
+        },
+        [newListMenuOptions, openMenu, openSubMenu]
+    );
+
+    return (
+        <main onContextMenu={onContextMenu} className="flex h-full w-full flex-col">
+            <div className="flex items-center gap-6 px-11 py-6">
+                {isLoading ? (
+                    <>
+                        <div className="animate-shimmer h-10 w-40 shrink-0 rounded-lg bg-dark-700" />
+                        <div className="animate-shimmer h-10 w-64 shrink-0 rounded-lg bg-dark-700" />
+                        <div className="animate-shimmer h-10 w-20 shrink-0 rounded-lg bg-dark-700" />
+                    </>
+                ) : (
+                    <>
+                        <Link href={Routes.Project(projectId)}>
+                            <Button.Small icon={IconArrowBackUp} fit>
+                                Back
+                            </Button.Small>
+                        </Link>
+
+                        <h2 className="ts-2xl">{name}</h2>
+
+                        <MemberList.Large users={users} min={5} max={7} />
+
+                        <Button.Large icon={IconFilter} fit>
+                            Filter
+                        </Button.Large>
+
+                        <Button.Large icon={IconPlus} fit onClick={onCreateNewList}>
+                            Create New List
+                        </Button.Large>
+                    </>
+                )}
+            </div>
+
+            {isLoading ? (
+                <div className="h-full w-full px-11 pb-6">
+                    <div className="animate-shimmer h-full w-full shrink-0 rounded-lg bg-dark-700" />
+                </div>
+            ) : (
+                children
+            )}
+
+            <TaskModal />
+        </main>
+    );
+}
